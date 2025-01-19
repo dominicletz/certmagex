928a783fbcf200bd75005852f8c4769417cf9ee4
Dominic Letz <dominic.letz@berlin.de>
2025-01-19 13:22:33 +0100

    v1.1.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CertMagex.MixProject do
   use Mix.Project
 
-  @version "1.0.9"
+  @version "1.1.0"
   @name "CertMagex"
   @url "https://github.com/dominicletz/certmagex"
   @maintainers ["Dominic Letz"]
